fa246d490335f2719ecb623708922ea3b4723921
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-02-27 12:45:36 +0600

    UI: home page top bar with balance and log out button

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('login-btn').addEventListener('click',function(){
+ // console.log('Login Button Clicked')
+
+     // 1 get the mobile number
+    const numberInput = document.getElementById('input-number')
+    const contactNumber = numberInput.value
+    console.log(contactNumber);
+   
+    // 2 get the pin number
+    const pinInput = document.getElementById('input-pin')
+    const pin = pinInput.value
+    // console.log(pinNumber)
+
+    // 3 match the pin
+    if(contactNumber == '12345678900' && pin == '1234'){
+        // 3-1 ::: true alert >homepage
+        alert('Login Sussefully')
+        // window.location.replace('/home.html')
+        window.location.assign('/home.html')
+
+    }
+    else{
+        // 3-2::: false ::> alert > return
+        alert('Login Failed')
+        return;
+
+    }
+})
